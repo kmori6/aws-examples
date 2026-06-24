2291b8c31f09647a5f79ad14cc5bbdfaa8a0c97e
Kosuke Mori <kosuke.mori.5120@gmail.com>
2026-06-25 00:04:00 +0900

    add ec2

---
+++ examples/ec2/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my_ec2"
+  }
+}
